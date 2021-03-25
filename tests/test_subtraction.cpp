7efc251482906e12b74f8937ac86c4e7cd1aadba
maruncz <marun1@email.cz>
2021-03-25 14:56:37 +0100

    pridany testy

--- tests/test_subtraction.cpp
+++ tests/test_subtraction.cpp
@@ -1,8 +1,8 @@
-#include "test_addition.h"
+#include "test_subtraction.h"
 #include <random>
 
 template <std::uint8_t T_numBits, std::uint8_t T_denBits>
-void random_additions()
+void random_subtractions()
 {
     using q_t                                   = q<T_numBits, T_denBits>;
     auto                                   qmin = q_t::min().toDouble();
@@ -18,33 +18,32 @@ void random_additions()
         std::uniform_real_distribution<double> tmp_dist(min, max);
         q_t                                    f2(tmp_dist(generator));
         double                                 d2 = f2.toDouble();
-        q_t                                    f3 = f1 + f2;
-        double                                 d3 = d1 + d2;
+        q_t                                    f3 = f1 - f2;
+        double                                 d3 = d1 - d2;
+
+        if (std::abs(d3 - f3.toDouble()) >= f3.eps().toDouble())
+        {
+            std::cerr << "f1: " << f1.toDouble() << " f2: " << f2.toDouble()
+                      << std::endl;
+        }
+
         ASSERT_NEAR(d3, f3.toDouble(), f3.eps().toDouble());
     }
 }
 
-TEST(operations, addition)
+TEST(operations, subtraction) {}
+
+TEST(operations, subtraction17)
 {
-    {
-        q<2, 6> f1(1.9);
-        q<4, 4> f2(5.4);
-        q<4, 4> f3 = f2 + q<4, 4>(f1);
-        ASSERT_NEAR(f3.toDouble(), 5.4 + 1.9, f3.eps().toDouble());
-    }
+    random_subtractions<1, 7>();
 }
 
-TEST(operations, addition17)
+TEST(operations, subtraction116)
 {
-    random_additions<1, 7>();
+    random_subtractions<1, 16>();
 }
 
-TEST(operations, addition116)
+TEST(operations, subtraction131)
 {
-    random_additions<1, 16>();
-}
-
-TEST(operations, addition131)
-{
-    random_additions<1, 31>();
+    random_subtractions<1, 31>();
 }
---
+++ tests/test_subtraction.h
@@ -0,0 +1,10 @@
+#ifndef TEST_SUBTRACTION_H
+#define TEST_SUBTRACTION_H
+
+#include "../q-format.h"
+#include <gmock/gmock-matchers.h>
+#include <gtest/gtest.h>
+
+class operations_subtraction;
+
+#endif // TEST_SUBTRACTION_H
