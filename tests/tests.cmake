16bc2c3f971f1e8d3aa1570e3331b67b2ae3d0a3
maruncz <marun1@email.cz>
2021-04-17 13:28:52 +0200

    uklid v cmake

--- tests/tests.cmake
+++
@@ -1,7 +0,0 @@
-file(GLOB test_srcs tests/*.h tests/*.cpp)
-list(FILTER test_srcs EXCLUDE REGEX ".*test-generator.cpp$")
-
-add_executable(tests ${test_srcs})
-target_include_directories(tests PRIVATE ${CMAKE_SOURCE_DIR}/q-format)
-
-add_executable(test-generator tests/test-generator.cpp)
