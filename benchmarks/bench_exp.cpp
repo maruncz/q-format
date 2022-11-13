69be1cb9637241ed3b8f84a36bd4a4801e4e558b
maruncz <marun1@email.cz>
2022-11-13 11:58:40 +0100

    added another benchmark

--- benchmarks/bench_exp.cpp
+++ benchmarks/bench_exp.cpp
@@ -1,14 +1,19 @@
-#include <q-format.h>
-#include <q-format-ops-exp.h>
-#include <benchmarktemplate.h>
+#include "q-format.h"
 #include <benchmarkbasic.h>
 #include <cmath>
 
-
-BENCHMARK(exp,float_std)
+BENCHMARK(exp, float_std)
 {
     float in = 5.0f;
     DoNotOptimize(in);
     auto res = std::exp(in);
+    DoNotOptimize(res);
+}
+
+BENCHMARK(exp, q_16_16)
+{
+    q<16, 16> in{5.0f};
+    DoNotOptimize(in);
+    auto res = q<16, 16>::exp(in);
     DoNotOptimize(res);
 }
